bc2ac1bc96b4a9bfb3941bbc2f06ac79a9576daa
James Sparks <ru_sparks@yahoo.com>
2025-09-09 20:21:03 -0700

    DOC

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our doc
+            Read our DOC
           </a>
         </div>
       </main>
