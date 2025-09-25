0996da866176a9ba1d36b5bec1a2559e93743b95
James Sparks <ru_sparks@yahoo.com>
2025-09-25 13:57:29 -0700

    Add connection info to db-status

--- pages/api/db-check.ts
+++ pages/api/db-check.ts
@@ -1,27 +1,20 @@
-// // pages/api/db-check.ts
-// import pool from '@/lib/db';
-// import type { NextApiRequest, NextApiResponse } from 'next';
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   try {
-//     const result = await pool.query('SELECT NOW()');
-//     res.status(200).json({ time: result.rows[0], message: 'Sparks Database connection successful' });
-//   } catch (err) {
-//     console.error('DB query failed', err);
-//     res.status(500).json({ error: 'Database connection failed' });
-//   }
-// }
-
 // pages/api/db-check.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getPool } from '@/lib/db';
+
+const environment = process.env.NODE_ENV;
+const dbUrl = process.env.DATABASE_URL;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const db = getPool();
     const { rows } = await db.query('select now() as now');
-    res.status(200).json({ time: rows[0], message: 'Sparks Database connection successful' });
+    res.status(200).json({ time: rows[0], message: `Sparks Database connection successful. Running in ${environment}. DATABASE_URL: ${dbUrl}` });
   } catch (err) {
-    res.status(500).json({ error: 'Database connection failed' });
+    res.status(500).json({ error: 'Database connection failed',
+       details: (err as Error).message, message: `Sparks Database connection failed. Running in ${environment}. DATABASE_URL: ${dbUrl}` });
   }
 }
+
+
+
